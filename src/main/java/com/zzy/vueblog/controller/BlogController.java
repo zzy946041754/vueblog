0798891bfb04e3ff496a518c2ffba7f1a0df0991
zzy <37695330+zzy946041754@users.noreply.github.com>
2021-02-07 10:42:55 +0800

    博客接口开发

--- src/main/java/com/zzy/vueblog/controller/BlogController.java
+++ src/main/java/com/zzy/vueblog/controller/BlogController.java
@@ -1,13 +1,60 @@
 package com.zzy.vueblog.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.Assert;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zzy.vueblog.common.Result;
+import com.zzy.vueblog.entity.Blog;
+import com.zzy.vueblog.service.BlogService;
+import com.zzy.vueblog.utils.ShiroUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 
 @RestController
-@RequestMapping("/blog")
 public class BlogController {
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService){
+        this.blogService = blogService;
+    }
+    @GetMapping("/blogs")
+    public Result blogs(Integer currentPage) {
+        if(currentPage == null || currentPage < 1) currentPage = 1;
+        Page page = new Page(currentPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("created"));
+        return Result.succ(pageData);
+    }
+    @GetMapping("/blog/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "该博客已删除！");
+        return Result.succ(blog);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/blog/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+        System.out.println(blog.toString());
+        Blog temp = null;
+        if(blog.getId() != null) {
+            temp = blogService.getById(blog.getId());
+            Assert.isTrue(temp.getUserId() == ShiroUtil.getProfile().getId(), "没有权限编辑");
+        } else {
+            temp = new Blog();
+            temp.setUserId(ShiroUtil.getProfile().getId());
+            temp.setCreated(LocalDateTime.now());
+            temp.setStatus(0);
+        }
+        BeanUtil.copyProperties(blog, temp, "id", "userId", "created", "status");
+        blogService.saveOrUpdate(temp);
+        return Result.succ(200,"操作成功", null);
+    }
 
 }
---
+++ src/main/java/com/zzy/vueblog/utils/ShiroUtil.java
@@ -0,0 +1,12 @@
+package com.zzy.vueblog.utils;
+
+import com.zzy.vueblog.shiro.AccountProfile;
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+    }
+
+}
